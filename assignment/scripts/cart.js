e351e05fe5105a921755edfe785f668c5a0f4245
Rafael Yasis <rafaelyasis@gmail.com>
2023-06-22 19:07:02 -0500

    finished assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,9 +1,42 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+/**
+ * 
+ * @param {string} item item should be a string
+ */
 
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+ addItem("Ball");
+ addItem("String");
+ //added two items to make sure listItems was working properly
+ console.log('What is in my Basket', basket);
 
+// create function called list items make it list the items
+
+ function listItems(basket) {
+    for (i = 0; i < basket.length; i++){
+        console.log(basket[i]);
+    }
+ }
+
+console.log("List of items in basket");
+listItems(basket);
+
+// create a function called empty
+
+function empty() {
+    basket = [];
+    console.log('basket is now empty');
+}
+
+empty(basket);
+console.log("basket should be empty", basket);
 
 
 
